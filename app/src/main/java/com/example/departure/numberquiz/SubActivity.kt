539f4d29ef35c064c980d916b31f167a23808872
nakamura <kotokoto08@gmail.com>
2018-05-08 12:23:03 +0900

    問題を出す処理

--- app/src/main/java/com/example/departure/numberquiz/SubActivity.kt
+++ app/src/main/java/com/example/departure/numberquiz/SubActivity.kt
@@ -4,7 +4,10 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.view.*
 import kotlinx.android.synthetic.main.activity_sub.*
+import java.lang.Math.random
+import java.util.*
 
 
 //SubActivityが開いたら
@@ -63,7 +66,41 @@ class SubActivity : AppCompatActivity(), View.OnClickListener {
     //問題を出す処理をするメソッド
     //問題が出されたら(questionメソッド)
     private fun question() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+//        ・もどるボタンを押せなくする(viewを使える、使えない)isEnable ture or false
+        btn_back.isEnabled = false
+//        ・こたえあわせボタンと数字ボタン,-,Cを押せるようにする
+        btn_ansCheck.isEnabled = true
+        button0.isEnabled = true
+        button1.isEnabled = true
+        button2.isEnabled = true
+        button3.isEnabled = true
+        button4.isEnabled = true
+        button5.isEnabled = true
+        button6.isEnabled = true
+        button7.isEnabled = true
+        button8.isEnabled = true
+        button9.isEnabled = true
+        buttonMinus.isEnabled = true
+        buttonClear.isEnabled = true
+
+//        ・問題の数字を表示ランダム表示
+        val random = Random()
+        val numLeft: Int = random.nextInt(100) + 1      //0から始まるので+1
+        val numRight: Int = random.nextInt(100) + 1
+        txv_left.text = numLeft.toString()
+        txv_right.text = numRight.toString()
+
+//        ・計算方法を+か-でランダム表示
+        when(random.nextInt(2) + 1){        //0から始まるので+1。１を＋、２をー
+            1 -> txv_enzanshi.text = "+"
+            2 -> txv_enzanshi.text = "-"
+        }
+
+//        ・前の問題で入力した自分の答えを消す
+        txv_answer.text = ""
+//        ・○ × 画像を見えないようにする     (viewを非表示　View.INVISIBLE、表示View.VISIBLE)
+        imageView.visibility = View.INVISIBLE
+
     }
 
     //こたえあわせ処理をするメソッド
@@ -74,7 +111,7 @@ class SubActivity : AppCompatActivity(), View.OnClickListener {
 
     //ボタンが押された時にやることをかく
     //数字ボタンを押したら
-    override fun onClick(p0: View?) {
+    override fun onClick(v: View?) {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
